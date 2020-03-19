16318c9ad3e50ba8399628404d5d930fc17fba61
Vasya231 <gg231589@gmail.com>
2020-03-19 13:58:50 +0300

    Добавлен запрос имени ползователя и вывод приветствия на экран

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import readlineSync from 'readline-sync';
+
+const askName = () => readlineSync.question('\nMay I have your name? ');
+
+const greet = (name) => {
+	if (name === '') {
+		console.log('Hello, anonymous!');
+		return 'anonymous';
+	};
+	console.log(`Hello, ${name}!`);
+	return name;
+};
+
+const greetings = () => greet(askName());
+
+export { greetings };
