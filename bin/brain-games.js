f2f9042f03d34f3efcc83330082359ab02a138ce
Vasya231 <gg231589@gmail.com>
2020-03-19 16:04:45 +0300

    Исправления по линтеру

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { greetings } from '../src/index.js';
+import greetings from '../src/index.js';
 
-console.log ('Welcome to the Brain Games!');
+console.log('Welcome to the Brain Games!');
 greetings();
